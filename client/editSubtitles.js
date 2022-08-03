7bf2a09dc8871799ec116bc0bbeefd38af73a78f
Darina Beneva <beneva.darina@gmail.com>
2022-08-04 00:43:15 +0300

    Fixed century formatting logic

--- client/editSubtitles.js
+++ client/editSubtitles.js
@@ -13,6 +13,7 @@ addTimingBtn.addEventListener('click', addTiming);
 function enablePrepareSubtitles() {
     if (subtitleEditor.value != '') {
         prepareSubsBtn.removeAttribute('disabled');
+        loadBtn.setAttribute('disabled', 'disabled');
     }
 }
 
@@ -21,6 +22,8 @@ async function loadScript() {
     subtitleEditor.value = script;
 
     loadBtn.setAttribute('disabled', 'disabled');
+    prepareSubsBtn.removeAttribute('disabled');
+    removeTitleBtn.removeAttribute('disabled');
 }
 
 function prepareSubtitles() {
@@ -31,7 +34,8 @@ function prepareSubtitles() {
 
     subtitleEditor.value = `${title}\n\r${subtitles}`;
 
-    prepareSubsBtn.setAttribute('disabled', 'disabled');
+    prepareSubsBtn.remove();
+    loadBtn.setAttribute('disabled', 'disabled');
     removeTitleBtn.removeAttribute('disabled');
     addTimingBtn.removeAttribute('disabled');
 }
@@ -60,29 +64,13 @@ function removeComments(script) {
 }
 
 function formatCenturies(script) {
-    let formattedScript = script.replace(/ I в.| I век| 1-ви в.| 1-ви век/g, ' 1. век');
-    formattedScript = formattedScript.replace(/ II в.| II век| 2-ри в.| 2-ри век/g, ' 2. век');
-    formattedScript = formattedScript.replace(/ III в.| III век| 3-ти в.| 3-ти век/g, ' 3. век');
-    formattedScript = formattedScript.replace(/ IV в.| IV век| 4-ти в.| 4-ти век/g, ' 4. век');
-    formattedScript = formattedScript.replace(/ V в.| V век| 5-ти в.| 5-ти век/g, ' 5. век');
-    formattedScript = formattedScript.replace(/ VI в.| VI век| 6-ти в.| 6-ти век/g, ' 6. век');
-    formattedScript = formattedScript.replace(/ VII в.| VII век| 7-ми в.| 7-ми век/g, ' 7. век');
-    formattedScript = formattedScript.replace(/ VIII в.| VIII век| 8-ми в.| 8-ми век/g, ' 8. век');
-    formattedScript = formattedScript.replace(/ IX в.| IX век| 9-ти в.| 9-ти век/g, ' 9. век');
-    formattedScript = formattedScript.replace(/ X в.| X век| 10-ти в.| 10-ти век/g, ' 10. век');
-    formattedScript = formattedScript.replace(/ XI в.| XI век| 11-ти в.| 11-ти век/g, ' 11. век');
-    formattedScript = formattedScript.replace(/ XII в.| XII век| 12-ти в.| 12-ти век/g, ' 12. век');
-    formattedScript = formattedScript.replace(/ XIII в.| XIII век| 13-ти в.| 13-ти век/g, ' 13. век');
-    formattedScript = formattedScript.replace(/ XIV в.| XIV век| 14-ти в.| 14-ти век/g, ' 14. век');
-    formattedScript = formattedScript.replace(/ XV в.| XV век| 15-ти в.| 15-ти век/g, ' 15. век');
-    formattedScript = formattedScript.replace(/ XVI в.| XVI век| 16-ти в.| 16-ти век/g, ' 16. век');
-    formattedScript = formattedScript.replace(/ XVII в.| XVII век| 17-ти в.| 17-ти век/g, ' 17. век');
-    formattedScript = formattedScript.replace(/ XVIII в.| XVIII век| 18-ти в.| 18-ти век/g, ' 18. век');
-    formattedScript = formattedScript.replace(/ XIX в.| XIX век| 19-ти в.| 19-ти век/g, ' 19. век');
-    formattedScript = formattedScript.replace(/ XX в.| XX век| 20-ти в.| 20-ти век/g, ' 20. век');
-    formattedScript = formattedScript.replace(/ XXI в.| XXI век| 21-ви в.| 21-ви век/g, ' 21. век');
+    const romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI'];
+    const patternRoman = / ([IVX]+)(-ти|-ми|-ри)* (в\.|век)/g;
+    const patternArabic = / ([0-9]+)(-ти|-ми|-ри)* (в\.|век)/g;
 
-    return formattedScript;
+    return script
+        .replace(patternRoman, (_, roman) => ` ${romanNumerals.findIndex(v => v == roman) + 1}. век`)
+        .replace(patternArabic, ' $1. век');
 }
 
 function removeSpacesAndEmoticons(script) {
